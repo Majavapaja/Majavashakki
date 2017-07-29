a13e000319af26f63a5cf82d5f242cfa8c0fc9ba
Sami Eklund <sami_eklund94@hotmail.com>
2017-07-29 18:17:14 +0300

    Added basic logic for move validating Added knight movement validation

--- src/server/logic/movementValidator.ts
+++ src/server/logic/movementValidator.ts
@@ -1,20 +1,77 @@
 import {Piece, Position} from '../../common/types';
 import Board from '../entities/board';
 
-export default class MovementValidator {
+class MovementValidator {
     constructor() {}
 
-    public static isValidMove(board: Board, start: Position, destination: Position): boolean {
+    public isValidMove(board: Board, start: Position, destination: Position): boolean {
+        // Check that start and destination are within board
+        if(!board.isWithinBoard(start) || !board.isWithinBoard(destination)) {
+            return false;
+        }
+
+        // Check that there is a piece at start position
         let startPiece: Piece = board.getPiece(start);
         if(!startPiece) return false;
-        
-        // start and destination are within Board
-        // There is a piece at start position
-        // Destination is valid position (empty or enemy)
-        //      Else Castling 
-        // Is movement valid for this piece?
-        //      Else En Passant
-        
+
+        // Check that destination is valid (different color or empty)
+        let destinationPiece: Piece = board.getPiece(destination);
+        if(destinationPiece && destinationPiece.color === startPiece.color) {
+            // TODO: Check castling
+            return false;
+        }
+
+        // Check that piece movement is valid
+        if(!this.checkMovement(board, startPiece, destination)) {
+            // TODO: Check En Passant
+            return false;
+        }
+
+        // Piece movement was valid
         return true;
     }
+
+    private checkMovement(board: Board, startPiece: Piece, destination: Position) {
+        switch(startPiece.type) {
+            case 'pawn':
+                return true;
+            case 'knight':
+                return this.knightMovement(board, startPiece, destination);
+            case 'rook':
+                return true;
+            case 'bishop':
+                return true;
+            case 'king':
+                return true;
+            case 'queen':
+                return true;
+        }
+    }
+
+    private pawnMovement(): boolean {
+        // TODO Pawn movement
+        return true;
+    }
+
+    private knightMovement(board: Board, startPiece: Piece, destination: Position): boolean {
+        // Knight can move one diagonally then one back to destination
+        let start = this.convertPosition(startPiece.position);
+        let dest = this.convertPosition(destination);
+
+        let rowDiff = Math.abs(dest.row - start.row);
+        let colDiff = Math.abs(dest.col - start.col);
+
+        if( (rowDiff === 2 && colDiff === 1) || (rowDiff === 1 && colDiff === 2)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private convertPosition(pos: Position) {
+        return {col: Board.cols.indexOf(pos.col), row: Board.rows.indexOf(pos.row)}
+    }
 }
+
+let movementValidator = new MovementValidator();
+export default movementValidator;
