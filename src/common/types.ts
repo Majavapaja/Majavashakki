d391abd164a3a90d6529feb0298b9d5c1e91541e
Sami Eklund <sami_eklund94@hotmail.com>
2017-07-29 20:03:58 +0300

    Reverted Position types back to just strings

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,6 +1,6 @@
 export interface Position {
-  col: "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h"
-  row: "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8"
+  col: string
+  row: string
 }
 
 export interface Piece {
