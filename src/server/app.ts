91611a34f6f1393f1a3b79189fa8bf38ee5ce160
Henry Heikkinen <rce@rce.fi>
2017-07-30 20:43:29 +0300

    Fix lints for app.ts

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,64 +1,58 @@
-// Import external modules
-import * as express from "express";
-import * as sio from "socket.io";
-// Import node core modules
 import * as http from "http";
 import * as path from "path";
-// Import custom components
+
+import * as express from "express";
+import * as sio from "socket.io";
+
 import {Game} from "./entities/GameRoom";
 import {GameRoomsRepository} from "./logic/GameRoomsRepository";
 import {UserStatesRepository} from "./logic/UserStatesRepository";
 
-// ################ Server base ################
-var app = express();
-var server = http.createServer(app);
-var io: SocketIO.Server = sio({transports:['websocket']});
+const app = express();
+const server = http.createServer(app);
+const io: SocketIO.Server = sio({transports: ["websocket"]});
 io.attach(server);
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-// ################ Routing ################
-app.use(express.static(path.resolve('dist/public')));
+app.use(express.static(path.resolve("dist/public")));
 
-// ################ Server constants ################
 const roomRepo = GameRoomsRepository.getInstance();
 const userStateRepo = UserStatesRepository.getInstance();
 
-// ################ Socket event bindings ################
-io.on('connection', function (socket: SocketIO.Socket) {
+io.on("connection", (socket: SocketIO.Socket) => {
 
-  socket.on("new user", function (username: string) { // TODO make proper stuff when auth is introduced
+  socket.on("new user", (username: string) => {
     userStateRepo.createUser(username, socket, roomRepo.MainRoom);
   });
 
-  socket.on("fetch-games", function () {
+  socket.on("fetch-games", () => {
     socket.emit("update-games", roomRepo.getAvailableGames());
-  })
+  });
 
-  socket.on("create-game", function (title: string) {
+  socket.on("create-game", (title: string) => {
     roomRepo.createRoom(title, userStateRepo.getState(socket.id));
   });
 
-  socket.on("join-game", function (roomTitle){
+  socket.on("join-game", (roomTitle) => {
     roomRepo.joinRoom(roomTitle, userStateRepo.getState(socket.id));
   });
 
-  socket.on('move', (data) => {
+  socket.on("move", (data) => {
     const game = roomRepo.getGameRoom(userStateRepo.getState(socket.id).currentRoom);
-    //TODO: Change parameter names to data.from to data.start and data.dest to data.destiantion
     const result = game.move(data.from, data.dest);
 
     switch (result.kind) {
     case "error":
-        return socket.emit('move_result', result);
+        return socket.emit("move_result", result);
     case "success":
-        return io.to(game.title).emit('move_result', result);
+        return io.to(game.title).emit("move_result", result);
     }
   });
 
 });
 
-module.exports = () => {
-  server.listen(port, function () {
-    console.log('Server listening at port %d', port);
+export default () => {
+  server.listen(port, () => {
+    console.log("Server listening at port %d", port);
   });
-}
+};
