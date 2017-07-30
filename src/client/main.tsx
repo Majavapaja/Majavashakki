e2c0b691c44b6c0b143ba4e391c97e979cfa7f9d
Henry Heikkinen <rce@rce.fi>
2017-07-30 21:11:24 +0300

    Wrap both LoginView and GameView inside App component

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -3,19 +3,13 @@ import * as ReactDOM from "react-dom";
 import * as io from "socket.io-client";
 
 import makeInitialState from '../common/initial-state';
-import GameView from './gameview';
-import LoginView from './LoginView';
+import App from './App';
 
 (function() {
   const socket: SocketIOClient.Socket = io();
 
   ReactDOM.render(
-    <LoginView socket={socket} />,
-    document.querySelector("#loginAndLobby")
-  )
-
-  ReactDOM.render(
-    <GameView pieces={makeInitialState()} socket={socket} />,
-    document.querySelector('.game.page')
+    <App socket={socket} />,
+    document.querySelector("#app")
   )
 })();
