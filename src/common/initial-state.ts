b9881c9cd40f8604ee6d282b3dfed8d74e25fdf8
Henry Heikkinen <rce@rce.fi>
2017-07-30 18:33:50 +0300

    Swap white to bottom and black on top

--- src/common/initial-state.ts
+++ src/common/initial-state.ts
@@ -3,112 +3,112 @@ import {Piece} from './types'
 const INITIAL_STATE: [Piece] = [
   {
     "type": "pawn",
-    "color": "white",
+    "color": "black",
     "position": {
       "row": "7",
       "col": "a"
     }
   }, {
     "type": "pawn",
-    "color": "white",
+    "color": "black",
     "position": {
       "row": "7",
       "col": "b"
     }
   }, {
     "type": "pawn",
-    "color": "white",
+    "color": "black",
     "position": {
       "row": "7",
       "col": "c"
     }
   }, {
     "type": "pawn",
-    "color": "white",
+    "color": "black",
     "position": {
       "row": "7",
       "col": "d"
     }
   }, {
     "type": "pawn",
-    "color": "white",
+    "color": "black",
     "position": {
       "row": "7",
       "col": "e"
     }
   }, {
     "type": "pawn",
-    "color": "white",
+    "color": "black",
     "position": {
       "row": "7",
       "col": "f"
     }
   }, {
     "type": "pawn",
-    "color": "white",
+    "color": "black",
     "position": {
       "row": "7",
       "col": "g"
     }
   }, {
     "type": "pawn",
-    "color": "white",
+    "color": "black",
     "position": {
       "row": "7",
       "col": "h"
     }
   }, {
     "type": "rook",
-    "color": "white",
+    "color": "black",
     "position": {
       "row": "8",
       "col": "a"
     }
   }, {
     "type": "knight",
-    "color": "white",
+    "color": "black",
     "position": {
       "row": "8",
       "col": "b"
     }
   }, {
     "type": "bishop",
-    "color": "white",
+    "color": "black",
     "position": {
       "row": "8",
       "col": "c"
     }
   }, {
     "type": "queen",
-    "color": "white",
+    "color": "black",
     "position": {
       "row": "8",
       "col": "d"
     }
   }, {
     "type": "king",
-    "color": "white",
+    "color": "black",
     "position": {
       "row": "8",
       "col": "e"
     }
   }, {
     "type": "bishop",
-    "color": "white",
+    "color": "black",
     "position": {
       "row": "8",
       "col": "f"
     }
   }, {
     "type": "knight",
-    "color": "white",
+    "color": "black",
     "position": {
       "row": "8",
       "col": "g"
     }
   }, {
     "type": "rook",
-    "color": "white",
+    "color": "black",
     "position": {
       "row": "8",
       "col": "h"
@@ -118,112 +118,112 @@ const INITIAL_STATE: [Piece] = [
 
   {
     "type": "pawn",
-    "color": "black",
+    "color": "white",
     "position": {
       "row": "2",
       "col": "a"
     }
   }, {
     "type": "pawn",
-    "color": "black",
+    "color": "white",
     "position": {
       "row": "2",
       "col": "b"
     }
   }, {
     "type": "pawn",
-    "color": "black",
+    "color": "white",
     "position": {
       "row": "2",
       "col": "c"
     }
   }, {
     "type": "pawn",
-    "color": "black",
+    "color": "white",
     "position": {
       "row": "2",
       "col": "d"
     }
   }, {
     "type": "pawn",
-    "color": "black",
+    "color": "white",
     "position": {
       "row": "2",
       "col": "e"
     }
   }, {
     "type": "pawn",
-    "color": "black",
+    "color": "white",
     "position": {
       "row": "2",
       "col": "f"
     }
   }, {
     "type": "pawn",
-    "color": "black",
+    "color": "white",
     "position": {
       "row": "2",
       "col": "g"
     }
   }, {
     "type": "pawn",
-    "color": "black",
+    "color": "white",
     "position": {
       "row": "2",
       "col": "h"
     }
   }, {
     "type": "rook",
-    "color": "black",
+    "color": "white",
     "position": {
       "row": "1",
       "col": "a"
     }
   }, {
     "type": "knight",
-    "color": "black",
+    "color": "white",
     "position": {
       "row": "1",
       "col": "b"
     }
   }, {
     "type": "bishop",
-    "color": "black",
+    "color": "white",
     "position": {
       "row": "1",
       "col": "c"
     }
   }, {
     "type": "queen",
-    "color": "black",
+    "color": "white",
     "position": {
       "row": "1",
       "col": "d"
     }
   }, {
     "type": "king",
-    "color": "black",
+    "color": "white",
     "position": {
       "row": "1",
       "col": "e"
     }
   }, {
     "type": "bishop",
-    "color": "black",
+    "color": "white",
     "position": {
       "row": "1",
       "col": "f"
     }
   }, {
     "type": "knight",
-    "color": "black",
+    "color": "white",
     "position": {
       "row": "1",
       "col": "g"
     }
   }, {
     "type": "rook",
-    "color": "black",
+    "color": "white",
     "position": {
       "row": "1",
       "col": "h"
