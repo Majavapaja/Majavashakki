fe52af7ed59faf0d6b1f9ffc17a93b42f8872c51
Sami Eklund <sami_eklund94@hotmail.com>
2017-07-30 19:02:59 +0300

    Fixed Board and MovementValidator to tslint standards Added ignore-same-line curly in tslint Renamed board and movementValidator to PascalCase

---
+++ src/server/entities/Board.ts
@@ -0,0 +1,48 @@
+import INITIAL_STATE from "../../common/initial-state";
+import {Piece, Position} from "../../common/types";
+import MovementValidator from "../logic/MovementValidator";
+
+export default class Board {
+    public static cols: string = "abcdefgh";
+    public static rows: string = "12345678";
+
+    public pieces: Piece[];
+    public moveHistory: Position[][];
+
+    constructor() {
+        this.pieces = Array.from(INITIAL_STATE);
+        this.moveHistory = [];
+    }
+
+    public move(start: Position, destination: Position): boolean {
+        if (!start || !destination) return false;
+
+        if (MovementValidator.isValidMove(this, start, destination)) {
+            // Move piece
+            this.removePiece(destination);
+            this.getPiece(start).position = destination;
+
+            this.moveHistory.push([start, destination]);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public removePiece(pos: Position): void {
+        const index: number = this.pieces.indexOf(this.getPiece(pos));
+        if (index !== -1) this.pieces.splice(index, 1);
+    }
+
+    public getPiece(pos: Position): Piece {
+        return this.pieces.find((piece) => this.comparePos(piece.position, pos));
+    }
+
+    public comparePos(a: Position, b: Position): boolean {
+        return a.row === b.row && a.col === b.col;
+    }
+
+    public isWithinBoard(pos: Position) {
+        return Board.cols.indexOf(pos.col) !== -1 && Board.rows.indexOf(pos.row) !== -1;
+    }
+}
