97b91167b847f3df09637f7c00e99345dff595e1
Alex <alex.engblomm@gmail.com>
2018-04-24 22:49:20 +0300

    Implemented disposable / using patterns, sync and async.

---
+++ src/server/components/disposable.ts
@@ -0,0 +1,25 @@
+export interface IDisposable {
+  dispose();
+}
+
+export function using<T extends IDisposable>(resource: T, func: (resource: T) => void) {
+  try {
+    func(resource);
+  } finally {
+      resource.dispose();
+  }
+}
+
+export interface IDisposableAsync {
+  init(): Promise<any>;
+  dispose();
+}
+
+export async function usingAsync<T extends IDisposableAsync>(resource: T, func: (resource: T) => Promise<any>) {
+  try {
+    await resource.init();
+    await func(resource);
+  } finally {
+      resource.dispose();
+  }
+}
