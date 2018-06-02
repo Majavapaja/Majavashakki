0d8be2638c6535782bbc3d7cbcee654d1f15b602
Henry Heikkinen <rce@rce.fi>
2018-06-02 14:54:35 +0300

    Prefix session secret env variable and follow the same naming style as other variables

--- src/server/session.ts
+++ src/server/session.ts
@@ -1,19 +1,19 @@
 import * as expressSession from "express-session";
 import * as ConnectMongo from "connect-mongo";
 import * as socketIoSession from "express-socket.io-session";
-import {MongooseClient} from "./data/MongooseClient"
+import {MongooseClient} from "./data/MongooseClient";
 
-const SESSION_SECRET = process.env.SESSION_SECRET
-if (!SESSION_SECRET) throw new Error("Environment variable SESSION_SECRET missing")
+const MajavashakkiSessionSecret = process.env.MajavashakkiSessionSecret;
+if (!MajavashakkiSessionSecret) throw new Error("Environment variable 'MajavashakkiSessionSecret' missing");
 
 export const getSession = (x: any): any =>
   x.session ? x.session : x.handshake.session;
 
 export const enableSessions = (app: any, io: SocketIO.Server) => {
-  const MongoStore = ConnectMongo(expressSession)
+  const MongoStore = ConnectMongo(expressSession);
   const s = expressSession({
     store: new MongoStore({ mongooseConnection: MongooseClient.getConnection() }),
-    secret: SESSION_SECRET,
+    secret: MajavashakkiSessionSecret,
     resave: false,
     saveUninitialized: true,
   });
