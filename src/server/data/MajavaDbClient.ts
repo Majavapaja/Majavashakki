8723131be9da52d21a2547a55f5b35a1a73ec147
Henry Heikkinen <rce@rce.fi>
2018-08-11 18:57:40 +0300

    Possibly fix mongo performance issues :thinking:

--- src/server/data/MajavaDbClient.ts
+++ src/server/data/MajavaDbClient.ts
@@ -2,18 +2,22 @@ import { MongoClient, Collection } from "mongodb";
 import {IDisposableAsync} from "../components/disposable";
 import * as util from "util";
 
+const password = process.env.MajavaMongoPassword;
+const mongoConnectionStr = process.env.MajavashakkiMongoConnectionString;
+const url = !!password ? util.format(mongoConnectionStr, encodeURIComponent(password)) : "mongodb://localhost:27017";
+const clientPromise = MongoClient.connect(url)
+  .then(client => {
+    console.log("Connected to Mongo url", url)
+    return client
+  })
+
 export class MajavaDbClient implements IDisposableAsync {
   public static readonly UserCollection: string = "ApplicationUsers";
   private client: MongoClient;
   private readonly dbName = "Majavashakki";
 
   public async init() {
-    const password = process.env.MajavaMongoPassword;
-    const mongoConnectionStr = process.env.MajavashakkiMongoConnectionString;
-    const url = !!password ? util.format(mongoConnectionStr, encodeURIComponent(password)) : "mongodb://localhost:27017";
-    console.log("MajavaDbClient Connecting to: " + url);
-    this.client = await MongoClient.connect(url);
-    console.log("MajavaDbClient Connected successfully to server");
+    this.client = await clientPromise
   }
 
   public getCollection<T>(collection: string): Collection<T> {
@@ -22,6 +26,5 @@ export class MajavaDbClient implements IDisposableAsync {
   }
 
   public dispose() {
-    this.client.close();
   }
 }
