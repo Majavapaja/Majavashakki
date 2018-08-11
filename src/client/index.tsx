b1938de29902e53ed728294d4f32705e356e300d
Sami Eklund <sami.eklund@cloudriven.fi>
2018-08-11 09:01:42 +0300

    Removed unused import and the puolipisteet

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,18 +1,16 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import * as io from "socket.io-client";
-
-import makeInitialState from "../common/initial-state";
-import App from "./App";
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
+import * as io from "socket.io-client"
+import App from "./App"
 
 (() => {
-  const socket: SocketIOClient.Socket = io();
+  const socket: SocketIOClient.Socket = io()
 
   ReactDOM.render(
     <MuiThemeProvider>
       <App socket={socket} />
     </MuiThemeProvider>,
     document.querySelector("#app"),
-  );
-})();
+  )
+})()
