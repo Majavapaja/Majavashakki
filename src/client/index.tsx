3279febfb805a4b46475b87d6849335212cfbca4
Sami Eklund <sami_eklund94@hotmail.com>
2018-09-24 21:24:49 +0300

    Added theme and theme provider for mui v1

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
+import V0MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import theme from "./mui-theme";
 import * as io from "socket.io-client"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App"
@@ -10,8 +12,10 @@ import App from "./App"
 
   ReactDOM.render(
     <BrowserRouter>
-      <MuiThemeProvider>
-        <App socket={socket} />
+      <MuiThemeProvider theme={theme}>
+        <V0MuiThemeProvider>
+          <App socket={socket} />
+        </V0MuiThemeProvider>
       </MuiThemeProvider>
     </BrowserRouter>,
     document.querySelector("#app"),
