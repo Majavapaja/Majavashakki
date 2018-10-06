3338ba53fa09a752c72a128751024baeced0ce02
Henry Heikkinen <rce@rce.fi>
2018-10-06 20:15:09 +0300

    Remove another sudo...

--- .circleci/circleci.sh
+++ .circleci/circleci.sh
@@ -14,7 +14,7 @@ apt-get install -y git gnupg python3-pip python3-venv curl
 
 # Install nodejs for running tests
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
-sudo apt-get install -y nodejs
+apt-get install -y nodejs
 
 # Run tests
 npm ci
