1559b7703ac5ae8bac6f39282fd06e6a29007041
Niklas Engblom <niklas.engblom@cloudriven.fi>
2018-10-06 15:24:24 +0300

    Added base styles and structure for login view

--- src/client/LoginView.tsx
+++ src/client/LoginView.tsx
@@ -1,17 +1,37 @@
 import * as React from "react";
 import { withRouter } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
 import RaisedButton from "material-ui/RaisedButton";
 
-class LoginView extends React.Component {
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        height: '100vh',
+        justifyContent: 'center'
+        alignItems: 'center'
+    },
+    loginContainer: {
+        width: 500,
+        padding: 25
+    }
+})
+
+class LoginView extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
     }
 
     public render() {
+        const { classes } = this.props
         return (
-            <RaisedButton label="Facebook" href="/authFacebook" />
+            <div className={classes.root}>
+                <Paper className={classes.loginContainer}>
+                    <RaisedButton label="Facebook" href="/authFacebook" />
+                </Paper>
+            </div>
         );
     }
 }
 
-export default withRouter(LoginView);
+export default withStyles(styles)(withRouter(LoginView));
