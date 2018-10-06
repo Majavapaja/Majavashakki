2ed2ae2a142f3c1fff37179fa95a4db940a9ea35
Henry Heikkinen <rce@rce.fi>
2018-10-06 17:51:51 +0300

    Fix mockgoose tests

--- test/data/User.spec.ts
+++ test/data/User.spec.ts
@@ -4,42 +4,39 @@ import {Mockgoose} from "../../node_modules/mockgoose/built/mockgoose";
 import * as assert from "assert";
 
 const mockgoose = new Mockgoose(mongoose);
-var User: IUserModel;
+const User = mongoose.model('User', UserSchema) as IUserModel;
 
 describe("User", () => {
-
     beforeEach(async () => {
         await mockgoose.prepareStorage();
         await mongoose.connect("mongodb://localhost/test");
-        User = mongoose.model('User', UserSchema) as IUserModel;
     });
+
+    afterEach(async () => {
+        await mockgoose.helper.reset()
+        await mongoose.disconnect()
+        mockgoose.mongodHelper.mongoBin.childProcess.kill("SIGINT")
+    })
 
     describe("findOrCreate", () => {
         it("should create user", async () => {
-
             await User.findOrCreate("M4T4L4");
 
-            var users = await User.find().exec();
+            const users = await User.find().exec();
             assert.equal(1, users.length, "users");
-
         });
     });
 
     describe("addGame", () => {
         it("should add game for logged in user", async () => {
+            let user = await User.findOrCreate("M4T4L4");
 
-            var Usr = User;
-            var user = await Usr.findOrCreate("M4T4L4");
-
-            Usr.updateName(user._id, "HerraMatala");
-            await Usr.addGame(user._id, "P3L1");
-            user = await Usr.findOrCreate("M4T4L4");
+            User.updateName(user._id, "HerraMatala");
+            await User.addGame(user._id, "P3L1");
+            user = await User.findOrCreate("M4T4L4");
 
             assert.equal(user.games.length, 1, "added game");
             assert.equal(user.games[0], "P3L1", "game title");
-
-            // https://github.com/Mockgoose/Mockgoose/issues/71
-
         });
     });
 });
