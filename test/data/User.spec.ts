433e71478105943595e351c41e6bd21be8904eef
riipah <riipah@gmail.com>
2018-10-06 13:45:25 +0300

    test

--- test/data/User.spec.ts
+++ test/data/User.spec.ts
@@ -1,24 +1,40 @@
-import {User, IUserModel} from "../../src/server/Data/User";
+import {IUserModel} from "../../src/server/Data/User";
 import * as mongoose from "mongoose";
-import {Mockgoose} from "mockgoose";
+import {Mockgoose} from "../../node_modules/mockgoose/built/mockgoose";
 import * as assert from "assert";
 
+var mockgoose: Mockgoose;
+
 describe("User", () => {
-    describe("addGame", () => {
-        it("should add game for logged in user", done => {
 
-            var mockgoose = new Mockgoose(mongoose);
+    beforeEach(() => {
+        const mong = new mongoose.Mongoose();
+        mockgoose = new Mockgoose(mong);
+    });
 
-            mockgoose.prepareStorage().then(async () => {
-                var Usr = mongoose.model('User') as IUserModel;
-                const user = await Usr.findOrCreate("P3L1");
-                await Usr.addGame(user._id, "peli");
+    describe("findOrCreate", () => {
+        it("should create user", done => {
 
-                assert.equal(user.games.length, 1, "added game");
-                assert.equal(user.games[0], "P3L1", "game title");
-                done();
-            });
             done();
+
+        });
+    });
+
+    describe("addGame", () => {
+        it("should add game for logged in user", async () => {
+            console.log("prepare");
+            await mockgoose.prepareStorage();
+            console.log("after prepare");
+            var Usr = mongoose.model('User') as IUserModel;
+            console.log("usr");
+            const user = await Usr.findOrCreate("M4T4L4");
+            user.name = "HerraMatala";
+            console.log("findOrCreate");
+            await Usr.addGame(user._id, "P3L1");
+            console.log("assert");
+
+            assert.equal(user.games.length, 1, "added game");
+            assert.equal(user.games[0], "PPPPP", "game title");
 
         });
     });
