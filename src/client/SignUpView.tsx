867a5024b2894412363f940bd458db3fbdcffe87
Sami Eklund <sami_eklund94@hotmail.com>
2018-10-07 21:55:19 +0300

    Signup form can now handle input

--- src/client/SignUpView.tsx
+++ src/client/SignUpView.tsx
@@ -4,6 +4,7 @@ import { withStyles, createStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
 
 import Majava from "./Majava";
 
@@ -21,69 +22,116 @@ const styles = createStyles({
         justifyContent: "center",
         flexDirection: "column",
         alignItems: "center"
+    },
+    button: {
+        margin: "10px 0"
     }
 })
 
 class SignUpView extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
+        this.state = {
+            isLoading: false,
+            error: ""
+        }
     }
 
     public render() {
         const { classes } = this.props
+
+        const form = (
+            <React.Fragment>
+                <TextField
+                    id="email"
+                    label="Email"
+                    type="email"
+                    margin="normal"
+                    onChange={this.handleInputChange}
+                />
+                <TextField
+                    id="username"
+                    label="Username"
+                    margin="normal"
+                    onChange={this.handleInputChange}
+                />
+                <TextField
+                    id="password"
+                    label="Password"
+                    type="password"
+                    margin="normal"
+                    onChange={this.handleInputChange}
+                />
+                <TextField
+                    id="passwordConfirm"
+                    label="Confirm password"
+                    type="password"
+                    margin="normal"
+                    onChange={this.handleInputChange}
+                />
+                <Button
+                    variant="raised"
+                    color="primary"
+                    onClick={this.handleSubmit}
+                    className={classes.button}
+                >
+                    <Typography color="inherit">Register</Typography>
+                </Button>
+            </React.Fragment>
+        )
+
         return (
             <div className={classes.root}>
                 <Paper className={classes.formContainer}>
-                    <Majava />
-                    <TextField
-                        id="email"
-                        label="Email"
-                        margin="normal"
-                    />
-                    <TextField
-                        id="password"
-                        label="Password"
-                        type="password"
-                        margin="normal"
-                    />
-                    <TextField
-                        id="password-confirm"
-                        label="Gonfirm Bassword"
-                        type="password"
-                        margin="normal"
-                    />
-                    <TextField
-                        id="username"
-                        label="Name"
-                        margin="normal"
-                    />
-                    <TextField
-                        id="library"
-                        label="Library card number"
-                        margin="normal"
-                    />
-                    <TextField
-                        id="ssn"
-                        label="Social security number"
-                        margin="normal"
-                    />
-                    <TextField
-                        id="soulid"
-                        label="Soul id"
-                        margin="normal"
-                    />
-                    <TextField
-                        id="dog"
-                        label="Your dogs middlename"
-                        margin="normal"
-                    />
-                    <Button variant="raised" color="primary">
-                        Sell your soul
-                    </Button>
+                    <Majava animation={this.state.isLoading && "spin"}/>
+                    <Typography color="error">{this.state.error}</Typography>
+                    {!this.state.isLoading && form}
                 </Paper>
             </div>
         );
     }
+
+    private handleSubmit = () => {
+        if (this.state.password !== this.state.passwordConfirm) {
+            this.setState({
+                error: "Passwords don't match D:"
+            })
+        } else {
+            this.setState({
+                isLoading: true
+            })
+            registerUser(this.state).then(() => {
+                console.log("Forward user to lobby or login or whatever")
+                this.setState({
+                    isLoading: false
+                })
+            }).catch(error => {
+                console.error("Couldn't register user. ", error)
+                this.setState({
+                    isLoading: false,
+                    error: "Error when registering user. Please try again."
+                })
+            })
+        }
+    }
+
+    private handleInputChange = event => {
+        const target = event.target;
+        const value = target.type === "checkbox" ? target.checked : target.value;
+        const id = target.id;
+
+        this.setState({
+          [id]: value
+        });
+    }
+}
+
+const registerUser = (formData) => {
+    return new Promise((resolve, reject) => {
+        console.log("Submitted: " + JSON.stringify(this.state));
+        // TODO Post data to server
+        setTimeout(resolve, 3500)
+    })
 }
 
 export default withStyles(styles)(withRouter(SignUpView));
