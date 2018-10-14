29e06bd8024b090d5a9029854b493c39924d71bf
Sami Eklund <sami_eklund94@hotmail.com>
2018-10-14 19:56:02 +0300

    Fixed game initial state

--- src/common/initial-state.ts
+++ src/common/initial-state.ts
@@ -16,8 +16,8 @@ const state: Majavashakki.IPiece[] = [
   blackPiece(Majavashakki.PieceType.Pawn, "b7"),
   blackPiece(Majavashakki.PieceType.Pawn, "c7"),
   blackPiece(Majavashakki.PieceType.Pawn, "d7"),
-  blackPiece(Majavashakki.PieceType.Pawn, "f7"),
   blackPiece(Majavashakki.PieceType.Pawn, "e7"),
+  blackPiece(Majavashakki.PieceType.Pawn, "f7"),
   blackPiece(Majavashakki.PieceType.Pawn, "g7"),
   blackPiece(Majavashakki.PieceType.Pawn, "h7"),
 
@@ -25,8 +25,8 @@ const state: Majavashakki.IPiece[] = [
   blackPiece(Majavashakki.PieceType.Knight, "b8"),
   blackPiece(Majavashakki.PieceType.Bishop, "c8"),
   blackPiece(Majavashakki.PieceType.Queen,  "d8"),
-  blackPiece(Majavashakki.PieceType.King,   "f8"),
-  blackPiece(Majavashakki.PieceType.Bishop, "e8"),
+  blackPiece(Majavashakki.PieceType.King, "e8"),
+  blackPiece(Majavashakki.PieceType.Bishop,   "f8"),
   blackPiece(Majavashakki.PieceType.Knight, "g8"),
   blackPiece(Majavashakki.PieceType.Rook,   "h8"),
 
@@ -34,8 +34,8 @@ const state: Majavashakki.IPiece[] = [
   whitePiece(Majavashakki.PieceType.Pawn, "b2"),
   whitePiece(Majavashakki.PieceType.Pawn, "c2"),
   whitePiece(Majavashakki.PieceType.Pawn, "d2"),
-  whitePiece(Majavashakki.PieceType.Pawn, "f2"),
   whitePiece(Majavashakki.PieceType.Pawn, "e2"),
+  whitePiece(Majavashakki.PieceType.Pawn, "f2"),
   whitePiece(Majavashakki.PieceType.Pawn, "g2"),
   whitePiece(Majavashakki.PieceType.Pawn, "h2"),
 
@@ -43,8 +43,8 @@ const state: Majavashakki.IPiece[] = [
   whitePiece(Majavashakki.PieceType.Knight, "b1"),
   whitePiece(Majavashakki.PieceType.Bishop, "c1"),
   whitePiece(Majavashakki.PieceType.Queen,  "d1"),
-  whitePiece(Majavashakki.PieceType.King,   "f1"),
-  whitePiece(Majavashakki.PieceType.Bishop, "e1"),
+  whitePiece(Majavashakki.PieceType.King,   "e1"),
+  whitePiece(Majavashakki.PieceType.Bishop, "f1"),
   whitePiece(Majavashakki.PieceType.Knight, "g1"),
   whitePiece(Majavashakki.PieceType.Rook,   "h1")
 
