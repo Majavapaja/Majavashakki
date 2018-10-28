cb2cc31115881a7b863e88d17c9adcde757e9b5d
Sami Eklund <sami_eklund94@hotmail.com>
2018-10-28 15:17:38 +0200

    Socket is now initiated only in views which use socket and after login

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,17 +3,14 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import theme from "./mui-theme";
-import io from "socket.io-client"
 import { BrowserRouter } from "react-router-dom"
 import App from "./routes/App"
 
 (() => {
-  const socket: SocketIOClient.Socket = io()
-
   ReactDOM.render(
     <BrowserRouter>
       <MuiThemeProvider theme={theme}>
-        <App socket={socket} />
+        <App />
       </MuiThemeProvider>
     </BrowserRouter>,
     document.querySelector("#app"),
---
+++ src/client/routes/socket.ts
@@ -0,0 +1,13 @@
+
+import io from "socket.io-client"
+
+let socket: SocketIOClient.Socket
+
+const connectSocket = () => {
+    socket = io()
+}
+
+export const getSocket = () => {
+    if (!socket) connectSocket()
+    return socket
+}
