1144b4196740977e770b7bd4bf6ed833eed02c19
Sami Eklund <sami_eklund94@hotmail.com>
2018-10-31 20:51:30 +0200

    Lobby view is now typed correctly and other minor improvements

--- src/client/routes/lobby/LobbyView.tsx
+++ src/client/routes/lobby/LobbyView.tsx
@@ -1,80 +1,101 @@
 import * as React from "react";
-import { withRouter } from "react-router-dom";
+import { withRouter, RouteComponentProps } from "react-router-dom";
 import TextField from "@material-ui/core/TextField";
 import GameList from "./GameList";
 import ApiService from "../../common/ApiService";
-import {getSocket} from "../socket"
+import {connectSocket} from "../socket"
+import { Theme, createStyles, withStyles, Typography, Button, WithStyles } from "@material-ui/core";
 
-class LobbyView extends React.Component<any, any> {
-    private socket: SocketIOClient.Socket;
+class LobbyView extends React.Component<ILobbyViewProps, ILobbyViewState> {
+  constructor(props: any) {
+    super(props);
 
-    constructor(props: any) {
-        super(props);
+    connectSocket()
 
-        this.socket = getSocket()
+    this.state = {
+      newRoomForm: {
+        name: ""
+      },
+      availableGames: [],
+      myGames: [],
+      error: ""
+    };
+  }
 
-        this.state = {
-            newRoomName: "",
-            availableGames: [],
-            myGames: []
-        };
+  public async componentDidMount() {
+    const [availableGames, myGames] = await Promise.all([
+      ApiService.read.availableGames(),
+      ApiService.read.myGames()
+    ]);
+
+    this.setState({ availableGames, myGames });
+  }
+
+  public onSubmitNewRoom = async (event) => {
+    event.preventDefault();
+
+    const gameTitle = this.cleanInput(this.state.newRoomForm.name);
+    if (gameTitle) {
+      const game = await ApiService.write.game(gameTitle);
+      this.setState({availableGames: [...this.state.availableGames, game] });
+      // TODO don't join game immediatly, instead push to my-games?
+      const result = await ApiService.write.joinGame(gameTitle);
+      this.props.history.push(`/game/${result.title}`)
     }
+  }
 
-    public async componentDidMount() {
-        const [availableGames, myGames] = await Promise.all([
-            ApiService.read.availableGames(),
-            ApiService.read.myGames()
-        ]);
+  public cleanInput(input: string): string {
+    return input.trim().replace("<", "").replace(">", "");
+  }
 
-        this.setState({availableGames, myGames});
-    }
+  public onInputChange = ({target}) => {
+    this.setState({
+      newRoomForm: {
+        ...this.state.newRoomForm,
+        [target.name]: target.value,
+      }
+    });
+  }
 
-    public onSubmitNewRoom = async (event) => {
-        event.preventDefault();
+  public render() {
+    const { classes } = this.props
 
-        const gameTitle = this.cleanInput(this.state.newRoomName);
-        if (gameTitle) {
-            const game = await ApiService.write.game(gameTitle);
-            this.setState({availableGames: [...this.state.availableGames, game] });
-            // TODO don't join game immediatly, instead push to my-games?
-            const result = await ApiService.write.joinGame(gameTitle);
-            this.props.history.push(`/game/${result.title}`)
-        }
-    }
+    return (
+      <div className={classes.root}>
+        {this.state.error && <Typography color="error" variant="h2">Error: {this.state.error}</Typography>}
+        <form onSubmit={this.onSubmitNewRoom} className={classes.newRoom}>
+          <TextField
+            name="name"
+            label="Room name"
+            onChange={this.onInputChange}
+          />
+          <Button variant="raised" color="primary">Create</Button>
+        </form>
 
-    public cleanInput(input: string): string {
-        return input.trim().replace("<", "").replace(">", "");
-    }
-
-    public onInputChange({target}) {
-        this.setState({[target.name]: target.value});
-    }
-
-    public render() {
-
-        const onInputChange = this.onInputChange.bind(this);
-
-        return (
-            <div className="room page">
-                <h2 id="roomWelcome">
-                    Hello! Welcome to Majavashakki.
-                    Please, join existing game or create a new one.
-                </h2>
-                <GameList games={this.state.availableGames} title="Available games" />
-                <GameList games={this.state.myGames} title="My games" />
-                {this.state.error && <p>Error: {this.state.error}</p>}
-                <div className="newRoomArea">
-                    <form onSubmit={this.onSubmitNewRoom}>
-                        Create new room:
-                        <TextField
-                            name="newRoomName"
-                            label="Room name"
-                            onChange={onInputChange}
-                        />
-                    </form>
-                </div>
-            </div>
-        );
-    }
+        <GameList games={this.state.myGames} title="My games" />
+        <GameList games={this.state.availableGames} title="Available games" />
+      </div>
+    );
+  }
 }
-export default withRouter(LobbyView);
+
+interface ILobbyViewProps extends RouteComponentProps<any>, WithStyles<typeof styles> {}
+interface ILobbyViewState {
+  newRoomForm: any,
+  availableGames: global.IGameRef[],
+  myGames: global.IGameRef[],
+  error: string,
+}
+
+const styles = (theme: Theme) => createStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  newRoom: {
+    display: "flex",
+    alignItems: "center",
+  }
+});
+
+export default withStyles(styles)(withRouter(LobbyView));
