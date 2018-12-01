789e659ed5b5721c7c1c10b62fdb0aabf6e866da
Niklas Engblom <niklas.engblom@cloudriven.fi>
2018-12-01 16:59:42 +0200

    Added filters for game lists

--- src/client/routes/lobby/GameList.tsx
+++ src/client/routes/lobby/GameList.tsx
@@ -1,12 +1,38 @@
 import * as React from "react";
 import { withRouter, RouteComponentProps } from "react-router-dom";
-import { List, ListItem, ListItemText, Paper, createStyles, Theme, Typography, Button, WithStyles, withStyles } from "@material-ui/core";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Paper,
+  createStyles,
+  Theme,
+  Typography,
+  Button,
+  WithStyles,
+  withStyles,
+  FormControl,
+  InputLabel,
+  Input,
+  InputAdornment
+} from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
+import FilterIcon from "@material-ui/icons/Search"
 import ApiService from "../../common/ApiService";
 
 class GameList extends React.Component<IGameListProps, any> {
   constructor(props: any) {
     super(props);
+
+    this.state = {
+      filter: ''
+    }
+  }
+
+  private handleFilterChange = (event) => {
+    this.setState({
+      filter: event.target.value.toLowerCase()
+    })
   }
 
   public render() {
@@ -19,16 +45,34 @@ class GameList extends React.Component<IGameListProps, any> {
           <Typography variant="headline">{this.props.title}</Typography>
           <Button onClick={this.props.openDialog}><AddIcon /> new room</Button>
         </div>
+        <FormControl>
+          <InputLabel htmlFor="game-filter">Filter</InputLabel>
+          <Input
+            id="game-filter"
+            type="text"
+            value={this.state.filter}
+            onChange={this.handleFilterChange}
+            endAdornment={
+              <InputAdornment position="end">
+                <FilterIcon />
+              </InputAdornment>
+            }
+          />
+        </FormControl>
         <List>
-          {games.map(game => (
-            <ListItem
-              key={game.ref}
-              button
-              onClick={() => this.onRoomClick(game.title)}
-            >
-              <ListItemText primary={game.title} />
-            </ListItem>
-          ))}
+          {
+            games
+            .filter(game => game.title.toLowerCase().includes(this.state.filter))
+            .map(game => (
+              <ListItem
+                key={game.ref}
+                button
+                onClick={() => this.onRoomClick(game.title)}
+              >
+                <ListItemText primary={game.title} />
+              </ListItem>
+            ))
+          }
         </List>
         {noGames && <Typography>No games available</Typography>}
       </Paper>
