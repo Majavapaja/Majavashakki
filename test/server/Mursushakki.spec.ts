dc3a60879236b5cb180f494e22d27b112d4370ec
Henry Heikkinen <rce@rce.fi>
2018-12-11 10:30:49 +0200

    Refactor API test

--- test/server/Mursushakki.spec.ts
+++ test/server/Mursushakki.spec.ts
@@ -8,11 +8,11 @@ const PORT = 3000
 
 describe("Mursushakki API", () => {
   let closeServer
-  let cookieJar
+  let http: HttpClient
 
   beforeEach(async () => {
     closeServer = await start(PORT)
-    cookieJar = request.jar()
+    http = mkHttpClient()
   })
 
   afterEach(async () => {
@@ -21,12 +21,12 @@ describe("Mursushakki API", () => {
   })
 
   it("should allow registerng and logging in", async () => {
-    const userBeforeLogin = await req("GET", "/api/user")
+    const userBeforeLogin = await http("GET", "/api/user")
     assert.strictEqual(userBeforeLogin, undefined)
 
-    await registerAndLogin("Mikko Mallikas", "mikko.mallikas@example.com", "password123")
+    await registerAndLogin(http, "Mikko Mallikas", "mikko.mallikas@example.com", "password123")
 
-    const user = await req("GET", "/api/user")
+    const user = await http("GET", "/api/user")
     assert.strictEqual(typeof user.id, "string")
     assert.strictEqual(user.name, "Mikko Mallikas")
     assert.strictEqual(user.email, "mikko.mallikas@example.com")
@@ -34,34 +34,40 @@ describe("Mursushakki API", () => {
 
   it("should return 401 on invalid login", async () => {
     try {
-      await req("POST", "/api/login", { email: "doesnotexist@example.com", password: "password123" })
+      await http("POST", "/api/login", { email: "doesnotexist@example.com", password: "password123" })
       assert.fail("Login should have thrown because of invalid credentials")
     } catch (e) {
       assert.strictEqual(e.name, "StatusCodeError")
       assert.strictEqual(e.statusCode, 401)
     }
   })
+})
 
-  async function registerAndLogin(name: string, email: string, password: string): Promise<any> {
-    const registerResult = await req("POST", "/api/user/register", { name, email, password })
-    assert.strictEqual(registerResult.status, "OK")
+type HttpClient = (method: "GET" | "POST", path: string, body?: any) => Promise<any>
 
-    try {
-      await req("POST", "/api/login", { email, password })
-      assert.fail("Login should have thrown because of redirect")
-    } catch (e) {
-      assert.strictEqual(e.name, "StatusCodeError")
-      assert.strictEqual(e.statusCode, 302)
-    }
-  }
+function mkHttpClient(): HttpClient {
+  const jar = request.jar()
 
-  async function req(method: "GET" | "POST", path: string, body?: any): Promise<any> {
+  return async (method, path, body) => {
     return await request({
       method,
       url: `http://localhost:${PORT}${path}`,
       body,
       json: true,
-      jar: cookieJar,
+      jar,
     })
   }
-})
+}
+
+async function registerAndLogin(http: HttpClient, name: string, email: string, password: string): Promise<any> {
+  const registerResult = await http("POST", "/api/user/register", { name, email, password })
+  assert.strictEqual(registerResult.status, "OK")
+
+  try {
+    await http("POST", "/api/login", { email, password })
+    assert.fail("Login should have thrown because of redirect")
+  } catch (e) {
+    assert.strictEqual(e.name, "StatusCodeError")
+    assert.strictEqual(e.statusCode, 302)
+  }
+}
