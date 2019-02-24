481ff001a47a9bc7e42879668b276a1426435c95
Sami Eklund <sami_eklund94@hotmail.com>
2019-02-24 21:16:52 +0200

    Fixed stalemate not working properly Minor refactors

--- src/common/logic/Draw.ts
+++ src/common/logic/Draw.ts
@@ -1,5 +1,5 @@
 import BoardBase from "common/BoardBase"
-import { PieceColor, PieceType } from "common/GamePieces"
+import { PieceColor } from "common/GamePieces"
 
 export const isDraw = (board: BoardBase, playerColor: PieceColor) => {
   if (isStaleMate(board, playerColor)) return true
@@ -10,28 +10,21 @@ export const isDraw = (board: BoardBase, playerColor: PieceColor) => {
 }
 
 const isStaleMate = (board: BoardBase, playerColor): boolean => {
-  const boardCopy: BoardBase = new BoardBase();
-  boardCopy.pieces = board.pieces.map(piece => piece.clone());
-
   // TODO: See if this could be optimized
-  for (let i = 0; i < board.pieces.length; i++) {
-    const piece = board.pieces[i]
-
+  for (const piece of board.pieces) {
     // Only check the requested players moves
-    if (piece.color !== playerColor) return false
+    if (piece.color !== playerColor) continue
 
-    for (let col = 0; col < BoardBase.cols.length; col++) {
-      for (let row = 0; row < BoardBase.rows.length; row++) {
-        const destination = { row: BoardBase.rows[row], col: BoardBase.cols[col] }
-        if (boardCopy.isValidMove(piece.position, destination)){
-          console.log('Cant be stalemate')
+    for (const col of BoardBase.cols) {
+      for (const row of BoardBase.rows) {
+        const destination = { row, col }
+        if (board.isValidMove(piece.position, destination)) {
           return false
         }
       }
     }
   }
 
-  console.log('Was stalemate')
   return true
 }
 
@@ -45,5 +38,5 @@ const thereIsntEnoughMaterialToDoCheckmate = (board: BoardBase): boolean => {
   // king and knight versus king
   // king and bishop versus king and bishop with the bishops on the same color.
 
-  return true
+  return false
 }
