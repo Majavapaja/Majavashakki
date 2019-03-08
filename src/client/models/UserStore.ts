ed50d72bf7f01ee0e3efc4a57216eaf7926c4638
Sami Saves <sami_eklund94@hotmail.com>
2019-03-08 20:07:35 +0200

    Added update method to user store

--- src/client/models/UserStore.ts
+++ src/client/models/UserStore.ts
@@ -1,5 +1,6 @@
 import { observable, action } from "mobx";
 import ApiService from "../common/ApiService";
+import { UserUpdateRequest } from "common/types";
 
 export default class UserStore {
 
@@ -18,8 +19,20 @@ export default class UserStore {
   @action
   public async login(email: string, password: string) {
     await this._api.write.login({email, password} as global.IUserContract);
-    var user = await this._api.read.user();
+    const user = await this._api.read.user();
+    console.log(user)
+
     this.name = user.name
     this.email = user.email
+  }
+
+  @action
+  public async update(email: string, name: string) {
+    const payload: UserUpdateRequest = { name, email }
+
+    await this._api.write.user(payload);
+
+    this.name = name
+    this.email = email
   }
 }
