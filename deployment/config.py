c1f1e34395db80b3c93f0ae3826c970d08be0e74
Henry Heikkinen <rce@rce.fi>
2019-04-24 17:22:58 +0300

    Cleanup

--- deployment/config.py
+++ deployment/config.py
@@ -7,8 +7,6 @@ class Azure:
 
 class Mongo:
   database_name = "Majavashakki"
-  cheap_database_name = "chess"
   collection_throughput = 400
-  throughput = collection_throughput
   system_indexes_collection = "undefined" # https://github.com/Automattic/mongoose/issues/6989
   collections = ["gamemodels", "sessions", "users", system_indexes_collection]
