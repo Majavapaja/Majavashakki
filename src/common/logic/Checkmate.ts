5f41f2ce2e913b87e3f073b3c0713157fbae6c58
Sami Saves <sami_eklund94@hotmail.com>
2019-04-24 19:09:12 +0300

    Removed todos getCheckingPiece and isDoubleCheck both check if it is a valid move to capture the king. Enpassant, castling can't capture a king and in promotion it doesn't matter

--- src/common/logic/Checkmate.ts
+++ src/common/logic/Checkmate.ts
@@ -93,9 +93,6 @@ function getCheckingPiece(board: BoardBase, king: King): Piece {
     for (const piece of board.pieces) {
         if (piece.color !== king.color) {
             if (piece.isValidMove(board, king.position)) {
-                // TODO: What if move was enPassant?
-                // TODO: What if move was castling?
-                // TODO: What if move was promotion?
                 return piece
             }
         }
@@ -110,9 +107,6 @@ function isDoubleCheck(board: BoardBase, king: King): boolean {
     for (const piece of board.pieces) {
         if (piece.color !== king.color) {
             if (piece.isValidMove(board, king.position)) {
-                // TODO: What if move was enPassant?
-                // TODO: What if move was castling?
-                // TODO: What if move was promotion?
                 checkingPieces++
             }
         }
