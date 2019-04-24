42365dbf82400855f3f218ab9e03de7dd5ebbc4b
Sami Saves <sami_eklund94@hotmail.com>
2019-04-24 16:45:58 +0300

    Added sum empty lines

--- test/logic/Check.spec.ts
+++ test/logic/Check.spec.ts
@@ -13,22 +13,26 @@ describe("Check", () => {
                 .then(board => moveSequence(board, [["b3", "b1"]]))
             promise.should.eventually.have.same.members(["move|check"]).notify(done)
         })
+
         it("should not allow king to move into check", done => {
             const promise = factory.build("board-check")
                 .then(board => moveSequence(board, [["c1", "b1"]]))
             promise.should.eventually.have.same.members(["error"]).notify(done)
         })
+
         it("should not allow king to move from check to check", done => {
             const promise = factory.build("board-check")
                 .then(board => moveSequence(board, [["b3", "b1"], ["c1", "d1"]]))
             promise.should.eventually.have.same.members(["move|check", "error"]).notify(done)
         })
+
         it("should allow king to capture checking piece", done => {
             const promise = factory.build("board-check")
                 .then(board => moveSequence(board, [["b3", "b1"], ["c1", "b1"]]))
             promise.should.eventually.have.same.members(["move|check", "capture"]).notify(done)
         })
-    }),
+    })
+
     describe("King in check", () => {
         it("should not allow king to capture checking piece if it causes check", done => {
             const promise = factory.build("board-king-in-check")
@@ -47,7 +51,8 @@ describe("Check", () => {
                 .then(board => moveSequence(board, [["a1", "b1"]]))
             promise.should.eventually.have.same.members(["error"]).notify(done)
         })
-    }),
+    })
+
     describe("Ohter pieces causing check", () => {
         it("Should not allow other pieces moving to cause check", done => {
             const promise = factory.build("board-check-from-own-move")
@@ -55,6 +60,7 @@ describe("Check", () => {
             promise.should.eventually.have.same.members(["error"]).notify(done)
         })
     })
+
     describe("Enpassant causes check", () => {
         it("should be able to check with enpassant", done => {
             const promise = factory.build("board-check-enpassant")
@@ -62,6 +68,7 @@ describe("Check", () => {
             promise.should.eventually.have.same.members(["move", "enpassant|check"]).notify(done)
         })
     })
+
     describe("Castling causes check", () => {
         it("should be able to check with castling", done => {
             const promise = factory.build("board-check-castling")
