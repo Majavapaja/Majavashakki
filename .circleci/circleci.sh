9a78d8cffa9d33c4da56d1ad3d139469ef6fdfc4
Henry Heikkinen <rce@rce.fi>
2019-05-01 14:14:55 +0300

    Make apt-get installs a bit less noisy

--- .circleci/circleci.sh
+++ .circleci/circleci.sh
@@ -9,12 +9,12 @@ if [ -z "$DEPLOYER_PRIVATE_GPG_KEY_BASE64" ]; then
   exit 1
 fi
 
-apt-get update
-apt-get install -y git gnupg python3-pip python3-venv curl
+apt-get -qq update
+apt-get -qq install -y git gnupg python3-pip python3-venv curl
 
 # Chrome dependencies
 # https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#chrome-headless-doesnt-launch-on-unix
-apt-get install -y \
+apt-get -qq install -y \
   gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 \
   libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 \
   libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 \
@@ -23,7 +23,7 @@ apt-get install -y \
 
 # Install nodejs for running tests
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
-apt-get install -y nodejs
+apt-get -qq install -y nodejs
 
 echo $DEPLOYER_PRIVATE_GPG_KEY_BASE64 | base64 --decode > private.key
 gpg --import private.key
