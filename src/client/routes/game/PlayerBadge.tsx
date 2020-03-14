653c9bfd104d21cc7a4dbfc5159cad692afac145
= <sami_eklund94@hotmail.com>
2020-03-14 16:38:29 +0200

    Fix indentation

--- src/client/routes/game/PlayerBadge.tsx
+++ src/client/routes/game/PlayerBadge.tsx
@@ -10,51 +10,51 @@ import * as Majavashakki from "../../../common/GamePieces"
 @inject((stores: IRootStore) => ({game: stores.app.game}))
 @observer
 class PlayerBadge extends React.Component<IPlayerBadgeProps, any> {
-    public render() {
-        const { classes, color, game } = this.props
+  public render() {
+    const { classes, color, game } = this.props
 
-        const player = color === Majavashakki.PieceColor.White ? game.playerWhite : game.playerBlack
+    const player = color === Majavashakki.PieceColor.White ? game.playerWhite : game.playerBlack
 
-        const rootClasses = classNames(
-            classes.root,
-            { active: game.currentTurn === color, winner: game.winner === color },
-        )
+    const rootClasses = classNames(
+      classes.root,
+      { active: game.currentTurn === color, winner: game.winner === color },
+    )
 
-        return (
-            <Chip
-                id={`${color}Badge`}
-                className={rootClasses}
-                avatar={<Avatar className={[classes.playerColor, color].join(" ")} />}
-                label={player ? player.name : "N/A"}
-            />
-        )
-    }
+    return (
+      <Chip
+        id={`${color}Badge`}
+        className={rootClasses}
+        avatar={<Avatar className={[classes.playerColor, color].join(" ")} />}
+        label={player ? player.name : "N/A"}
+      />
+    )
+  }
 }
 
 const styles = (theme: Theme) => ({
-    root: {
-        margin: "0 20px",
-        "&.active": {
-            backgroundColor: theme.palette.primary.main,
-            color: theme.palette.primary.contrastText,
-        },
-        "&.winner": {
-            backgroundColor: "green",
-            color: theme.palette.primary.contrastText,
-        },
+  root: {
+    margin: "0 20px",
+    "&.active": {
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.primary.contrastText,
     },
-    playerColor: {
-        width: 50,
-        height: 50,
-        border: "1px solid rgba(0,0,0,0.5)",
-        borderRadius: "50%",
-        "&.white": {
-            backgroundColor: "#FFF",
-        },
-        "&.black": {
-            backgroundColor: "#000",
-        },
+    "&.winner": {
+      backgroundColor: "green",
+      color: theme.palette.primary.contrastText,
     },
+  },
+  playerColor: {
+    width: 50,
+    height: 50,
+    border: "1px solid rgba(0,0,0,0.5)",
+    borderRadius: "50%",
+    "&.white": {
+      backgroundColor: "#FFF",
+    },
+    "&.black": {
+      backgroundColor: "#000",
+    },
+  },
 })
 
 interface IPlayerBadgeProps extends WithStyles<typeof styles> {
