3aaa621433fa483bf20fcececcd20979985c0b5f
Alex <alex.engblomm@gmail.com>
2020-03-15 10:40:11 +0200

    Get rid of unnecessary chai expect import

--- test/logic/EnPassant.spec.ts
+++ test/logic/EnPassant.spec.ts
@@ -1,6 +1,5 @@
 import factory from "./setup/BoardFactory";
 import {moveSequence} from "./setup/BoardHelper";
-import { expect } from "chai";
 import BoardBase from "common/BoardBase";
 
 describe("EnPassant", () => {
@@ -13,17 +12,17 @@ describe("EnPassant", () => {
 
   it("should allow en passant", () => {
     const results = moveSequence(subject, [["c2", "c4"], ["d4", "c3"]])
-    expect(results).to.eql(["move", "enpassant"])
+    results.should.eql(["move", "enpassant"])
   });
 
   it("should not allow en passant if pawn moves two steps", () => {
     const results = moveSequence(subject, [["c2", "c3"], ["c3", "c4"], ["d4", "c3"]])
-    expect(results).to.eql(["move", "move", "error"])
+    results.should.eql(["move", "move", "error"])
   });
 
   it("should not allow en passant if it is not the next move", () => {
     const results = moveSequence(subject, [["c2", "c4"], ["a7", "a6"], ["d4", "c3"]])
-    expect(results).to.eql(["move", "move", "error"])
+    results.should.eql(["move", "move", "error"])
   });
 
 });
